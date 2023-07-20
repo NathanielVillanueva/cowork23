d455dc1702d7e10289c845706830ab8fd93b38df
Nathaniel Villanueva <nathaniel.villanueva@getpantheon.com>
2023-07-20 16:17:16 +0800

    edited wp-config.php added wp-settings.php

--- wp-config.php
+++ wp-config.php
@@ -85,3 +85,9 @@ define( 'SCRIPT_DEBUG', true );
 
 /* That's all, stop editing! Happy Pressing. */
 
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
